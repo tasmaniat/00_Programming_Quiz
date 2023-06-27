8c6cbca69666fd2b1a60807e7c1eeb1238daba5e
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2023-06-27 21:55:31 +1200

    recycled rps_score and changed to fit in with quiz

---
+++ 06_game_scores.py
@@ -0,0 +1,36 @@
+# Math Quiz Component - scoring system
+
+# Rounds won will be calculated (total - incorrect - skipped)
+rounds_played = 0
+rounds_incorrect = 0
+rounds_skipped = 0
+
+# Results for testing purposes
+test_results = [True, False, False, True, True]
+
+# Play game
+for item in test_results:
+    rounds_played += 1
+
+    # Check answer
+
+    result = item
+
+    if result:
+        # Correct answer
+        print("Correct!")
+    else:
+        # Incorrect answer
+        rounds_incorrect += 1
+        print("Incorrect!")
+
+# Quick Calculations (stats)
+rounds_won = rounds_played - rounds_incorrect - rounds_skipped
+
+# End of game statements
+print()
+print('***** End Game Summary *****')
+print("Correct: {} \t|\t Incorrect: {} \t|\t Skipped: "
+      "{}".format(rounds_won, rounds_incorrect, rounds_skipped))
+print()
+print("Thanks for playing")
